4162229065f485a6a8b92476d3fdb9e54a6be1d5
Seke Nikola <32386925+SekeNikola@users.noreply.github.com>
2018-01-27 14:53:24 +0100

    Init Commit

---
+++ js/slide_in.js
@@ -0,0 +1,55 @@
+window.onwheel = wheelslide;
+
+
+// window scroll function
+function wheelslide(e)
+{
+  var moveFirst = document.getElementById('start_img');
+  var moveSecond = document.getElementById('move');
+  var moveFirstText = document.getElementById('first-text');
+  var moveSecondText = document.getElementById('second-text');
+
+ 
+  // scrolling downward first image
+  if(e.deltaY > 0)
+  {
+    if(moveFirst.style.left == '1000px' || moveFirst.style.left == '') moveFirst.style.left = '2000px';
+  }
+  else
+  {
+    if(moveFirst.style.left == '2000px') moveFirst.style.left = '1000px';
+  }
+//second img
+  if(e.deltaY > 0)
+  {
+    if(moveSecond.style.left == '2000px' || moveSecond.style.left == '') moveSecond.style.left = '1000px';
+  }
+  else
+  {
+    if(moveSecond.style.left == '1000px') moveSecond.style.left = '2000px';
+  }
+
+
+
+   
+// first text 
+if(e.deltaY > 0)
+  {
+    if(moveFirstText.style.left == '0' || moveFirstText.style.left == '') moveFirstText.style.left = '-500px';
+  }
+  else
+  {
+    if(moveFirstText.style.left == '-500px') moveFirstText.style.left = '0px';
+  }
+}
+
+//second text
+
+if(e.deltaY > 0)
+  {
+    if(moveSecondText.style.left == '-500px' || moveSecondText.style.left == '') moveSecondText.style.left = '0';
+  }
+  else
+  {
+    if(moveSecondText.style.left == '0') moveSecondText.style.left = '-500px';
+  }
